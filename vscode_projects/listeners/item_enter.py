53b0993029badc2b80c175f5c3891bda648d0dad
Jacob Deuchert <43518397+JacobDeuchert@users.noreply.github.com>
2025-10-29 15:44:52 +0100

    Fix syntax error in item_enter.py

--- vscode_projects/listeners/item_enter.py
+++ vscode_projects/listeners/item_enter.py
@@ -10,5 +10,5 @@ class ItemEnterEventListener(EventListener):
         """ Handles the click on an item of the extension """
         data = event.get_data()
 
-        code_executable = extension.preferences['code_executable_path'
+        code_executable = extension.preferences['code_executable_path']
         subprocess.run([code_executable, '--folder-uri', data['path']])
